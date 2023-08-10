807e068da5c478a0c8dde4b479e66655c4f4ef02
Yire Morlans <yire.m@icloud.com>
2023-08-10 11:13:49 -0400

    add sign out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, SignOutButton, useUser } from "@clerk/nextjs";
 
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  const user = useUser();
 
   return (
     <>
@@ -15,7 +17,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        {!user && <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />}
+        {!!user && <SignOutButton className="text-slate-100" />}
       </main>
     </>
   );
